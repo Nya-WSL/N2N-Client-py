a9d547b9357d99fe2bcc10444e1e6d2a6a488fab
狐日泽 <Sun_Set1102@outlook.com>
2022-08-27 21:06:19 +0800

    v1.0.4-dev1

---
+++ n2n-client-latest/Bat/update.bat
@@ -0,0 +1,6 @@
+timeout /T 2 /NOBREAK
+taskkill /F /IM n2n_client.exe
+move update\* .\
+move update\Ver\* .\Ver
+rd /s/q update\Ver
+pause
--- n2n_client.py
+++ n2n_client.py
@@ -11,33 +11,41 @@ from configparser import ConfigParser
 
 os.system('')
 
-if os.path.exists('n2n_client.log'):
-    os.remove('n2n_client.log') # del old log file
+LogConfig = ConfigParser()
+LogConfig.read('log.ini')
+LogFile = LogConfig.get('params', 'name')
+LogLevel = LogConfig.get('params', 'level')
+LogFormat = LogConfig.get('params', 'format')
+LogDatafmt = LogConfig.get('params', 'datafmt')
 
-logging.basicConfig(filename='n2n_client.log',level=logging.DEBUG,format="%(asctime)s - %(pathname)s - %(message)s",datefmt=\
-"%Y/%m/%d %H:%M:%S")
+if os.path.exists(LogFile):
+    os.remove(LogFile) # del old log file
+
+logging.basicConfig(filename=LogFile,level=LogLevel,format=LogFormat,datefmt=LogDatafmt)
 
 ConServer = ConfigParser()
-ConServer.read('n2n.ini')
+ConServer.read('url_config.ini')
 
-ConServerUrl = ConServer.get('setting','Server')
-ConServerRes = os.getcwd() + "./config.ini"
+ConServerUrl = ConServer.get('Server','server')
+# ConServerRes = os.getcwd() + "./config.ini"
 
-CsvUrl = "https://nya-wsl.osttsstudio.ltd/files/ServerList.csv"
-CsvRes = os.getcwd() + "./ServerList.csv"
-ConUrl = "https://nya-wsl.osttsstudio.ltd/files/n2n_config.ini"
-ConRes = os.getcwd() + "./Ver/server.ini"
-Zip_url = "https://nya-wsl.osttsstudio.ltd/files/n2n_update.zip"
+CsvUrl = ConServerUrl + ConServer.get('File','csvUrl')
+CsvRes = os.getcwd() + ConServer.get('File','csvRes')
+ConUrl = ConServerUrl + ConServer.get('File','conUrl')
+ConRes = os.getcwd() + ConServer.get('File','conRes')
+Zip_url = ConServerUrl + ConServer.get('File','zip_url')
 
 print('''
-┌───────────────────────────────────────────────────┐
-│              n2n_client            v1.0.4         │ 
-├───────────────────────────────────────────────────┤
-│       Project Nya-WSL.  All rights reserved.      │ 
-│ For more information,please visit:www.nya-wsl.com │
-├───────────────────────────────────────────────────┤
-│     Takahashiharuki&SHDocter       2022/07/14     │
-└───────────────────────────────────────────────────┘
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃                     N2N_Client                     ┃
+┃                                            v1.0.4  ┃ 
+┠────────────────────────────────────────────────────┨
+┃                A Project of Nya-WSL.               ┃ 
+┃ For more information,please visit: www.nya-wsl.com ┃
+┃    Copyright © 2021-2022. All rights reserved.     ┃
+┠────────────────────────────────────────────────────┨
+┃     Takahashiharuki & SHDocter      2022/08/27     ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 ''')
 
 try:
@@ -58,7 +66,11 @@ LocalVer = VerLocal.get('settings','version')
 ServerVer = VerServer.get('settings','version')
 
 try:
-    print(f'目前版本：{LocalVer}   最新版本：{ServerVer}')
+
+    print(f'''──────────────────────────────────────────────────────
+    目前版本：{LocalVer}   最新版本：{ServerVer}
+    ──────────────────────────────────────────────────────''')
+
 
     if LocalVer != ServerVer:
         print("\n\033[5;36;40m更新中，请等待。\033[0m\n")
@@ -93,20 +105,22 @@ try:
         print('''
 \n\033[5;36;40m
 目前已是最新版本！
-
+──────────────────────────────────────────────────────
 正在查询可用服务器，请稍后...
 \033[0m
 ''')
         if os.path.exists('./Ver/server.ini'):
             os.remove('./Ver/server.ini')
         request.urlretrieve(CsvUrl,CsvRes)
+        print('查询完成！')
 except:
     logging.debug(traceback.format_exc())
 
 
 try:
-    Name = input('请输入组名称(分组隔离，不在同一个组将无法组网)：')
-    print('-------------------------------------------')
+    Name = input('''──────────────────────────────────────────────────────
+    请输入组名称(分组隔离，不在同一个组将无法组网)：''')
+    print('──────────────────────────────────────────────────────')
 
     with open(r'./ServerList.csv',encoding='GB2312',errors='ignore') as csvfile:
         reader = csv.reader(csvfile)
@@ -117,23 +131,23 @@ try:
         address = [row[1] for row in reader]
 
         print('可用服务器列表：')
-        print('-------------------------------------------')
+        print('──────────────────────────────────────────────────────')
         for i in place:
             print("序号：%s 服务器：%s" % (place.index(i) + 1, i))
-        print('-------------------------------------------')
+        print('──────────────────────────────────────────────────────')
         number = int(input('请输入服务器序号，按Enter键结束：'))
         Server = address[number-1]
         print (f'''
 服务器地址:\033[5;36;40m{Server}\033[0m\n''')
 
     Assign = int(input('''
--------------------------------------------
+──────────────────────────────────────────────────────
 请选择IP分配方法
 
 1.手动分配
 2.自动分配(推荐)
 
--------------------------------------------
+──────────────────────────────────────────────────────
 
 请输入数字并按回车确认:'''))
     print('''
@@ -143,20 +157,16 @@ try:
 ''')
 
     if Assign == 1:
-        ip = int(input('请输入IP地址最后一组的数字，并按回车确认（例：71.94.86.13 只需要输入13）:'))
-        address = '71.94.86.%d'%ip
+        address = int(input('请输入IP地址，并按回车确认（例：127.0.0.1）:'))
         input(f'''
 IP:\033[5;36;40m{address}\033[0m\n
 如有误请关闭重新运行，无误请按回车确认''')
         echo = f"edge.exe -c {Name} -a {address} -l {Server}"
         os.system(echo)
     if Assign == 2:
-    # add4 = (random.sample(range(1,255),1)).pop()
-    # address = '71.94.86.%d'%add4
-    # input(f'IP:{address} 如有误请关闭重新运行，无误请按回车确认')
         echo = f"edge.exe -c {Name} -l {Server}"
         os.system(echo)
     elif Assign != 1 and 2:
-        input('参数错误！请关闭本窗口后重新启动！')
+        input('参数错误！请重新启动程式！')
 except:
     logging.debug(traceback.format_exc())
